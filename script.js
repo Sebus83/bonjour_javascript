ba651bc75d07abce24262ac271514c36817a47a8
Sebus83 <sebastien.herbiet@gmail.com>
2024-10-18 14:15:17 +0200

    optimisation_avec_commentaires_du_code_avec_heure_réelle

--- script.js
+++ script.js
@@ -10,18 +10,18 @@
 
 //encapsule le code précédent
 function sayHello() {
-  const firstName = prompt("Quel est ton prénom ?");
-  const date = new Date();
-  const hours = date.getHours();
+  const firstName = prompt("Quel est ton prénom ?"); // demande le prénom à l'utilisateur
+  const date = new Date();   //récupération de la date
+  const hours = date.getHours();  //récupération de l'heure
   
   let message;
-  if (hours <= 17) {
+  if (hours <= 17) {    //condition du message en fonction de l'heure
       message = `Bonjour ${firstName} !`;
   } else {
       message = `Bonsoir ${firstName} !`;
   }
   
-  document.querySelector('h1').innerText = message;
+  document.querySelector('h1').innerText = message; //envoie du message final dans h1 sur le index.html
   
 }
 
